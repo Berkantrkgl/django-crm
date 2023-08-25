f07671426b51f1ded59f8ed747356268f94ef22c
Berkantrkgl <e190501030@stud.tau.edu.tr>
2023-08-25 13:37:42 +0300

    Add tc no validation with regex

--- crm/models.py
+++ crm/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-from .validators import RegexValidator
+from django.core.validators import RegexValidator
 
 # Create your models here.
 
 class Customer(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     #tc kimlik kismi nasil yapilmali arastirilacak.
-    tc_no = models.IntegerField(editable=True)
+    tc_no = models.IntegerField(editable=True, validators=[
+        RegexValidator(r'^[1-9]{1}[0-9]{9}[02468]{1}$', message="Invalid Tc No ", code="invalid_number")], unique=True)
     first_name = models.CharField(max_length=50, editable=True)
     last_name =  models.CharField(max_length=50, editable=True)
     phone = models.CharField(max_length=15, editable=True)
